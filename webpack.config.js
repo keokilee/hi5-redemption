bfe63cd39ba295b8de192a882d3b598a3a6c75d6
George Lee <keoki.lee@gmail.com>
2016-01-09 15:20:05 -1000

    Initial commit of Vue implementation.

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  devServer: {
+    contentBase: `${process.cwd()}/dist`,
+    host: '0.0.0.0',
+    historyApiFallback: true,
+    noInfo: true
+  },
+  devtool: 'eval-source-map',
+  entry: {
+    app: './src/main.js'
+  },
+  eslint: {
+    formatter: require('eslint-friendly-formatter')
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      }, {
+        test: /\.js$/,
+        loader: 'babel!eslint',
+        exclude: /node_modules/
+      }, {
+        test: /\.json$/,
+        loader: 'json'
+      }, {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'url',
+        query: {
+          limit: 10000,
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  plugins: [ new HtmlWebpackPlugin({ title: 'HI-5 Redemption Centers' }) ],
+  output: {
+    path: `${process.cwd()}/dist`,
+    publicPath: '/',
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.vue'],
+    alias: {
+      'src': `${process.cwd()}/src`
+    }
+  },
+  vue: {
+    loaders: {
+      js: 'babel!eslint'
+    }
+  }
+}
